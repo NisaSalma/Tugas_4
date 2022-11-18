04d4cbf9ab6a136f85d084aec28565e45f6d9741
Khoerunisa Salma <115153134+NisaSalma@users.noreply.github.com>
2022-11-18 13:49:30 +0700

    Add files via upload

---
+++ tugas_4.py
@@ -0,0 +1,27 @@
+a = "pa,-lem,-bang"
+c = a.split(",")
+print(c)
+
+j = "ja,-kar,-ta"
+d = j.split(",")
+print(d)
+
+m = 'ma-du-ra'
+n = m.split('-')
+print(n)
+
+s = 'pa/dang'
+i = s.split('/')
+print(i)
+
+k = 'be|ka|si'
+o = k.split('|')
+print(o)
+
+h = 'ma+ka+sar'
+f = h.split('+')
+print (f)
+
+g = "go*ron*ta*lo"
+u = g.split("*")
+print (u)
